bede612e92b5ca05db22ba6057bc3e7b2dac687b
Natallia-Lu <kali.laska@mail.ru>
2023-02-09 22:59:32 +0100

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  
+  while (str.includes('()') || str.includes('[]') || str.includes('||') || str.includes('{}') || str.includes('12') || str.includes('34') || str.includes('56') || str.includes('77') || str.includes('88')) {
+    str = str.replace('()','')
+  str = str.replace('[]','')
+  str = str.replace('||','')
+  str = str.replace('{}','')
+  str = str.replace('12','')
+  str = str.replace('34','')
+  str = str.replace('56','')
+  str = str.replace('77','')
+  str = str.replace('88','')
+}
+
+  
+  if (str === '') {
+    return true
+  } else return false;
 }
